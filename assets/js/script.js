f844fbdb4de06bb972043e24df1002ea6ce5d6c4
PunkDevloPer <juandavidhernandezayala@gmail.com>
2023-09-21 19:31:19 +0200

    modificacion de script y estilos

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,25 +10,24 @@ document.addEventListener("DOMContentLoaded", function () {
         │   ├──index.html
         │   ├── Blog.html
         │   ├── Plataformas
-        │   │   ├── HackmyVM.html
-        │   │   ├── Tryhackme.html
-        │   │   ├── vulnhub.html
-        │   │   ├── Hackthebox.html
-        │   │   
-        │   ├── LINUX
-        │   │   ├── book.html
-        │   ├── PYTHON
-        │   │   ├── book.html
+        │       ├── HackmyVM.html
+        │       ├── Tryhackme.html
+        │       ├── vulnhub.html
+        │       ├── Hackthebox.html   
+        ├── LINUX
+        │     ├── book.html
+        ├── PYTHON
+        │     ├── book.html
         ├── usr
-            ├── Sobre mi
-            │   ├── cv.html`;
+        │    ├── Sobre mi
+        │      ├── cv.html
+
+        $>> Redireccionando ...`;
         typeMessage(directoryStructure, 0, consoleText, function () {
             menuItems.forEach(function (item, index) {
                 item.style.animationDelay = (index * 0.5) + "s";
                 item.style.display = "inline-block";
             });
-            consoleText2.style.opacity = "1"; /* Muestra la animación console-2 */
-
             // Redireccionar a una página después de que termine la animación
             setTimeout(function () {
                 window.location.href = "/Blog.html";
