3df187d946f8f28de4dec3d0a7f1ccbfeccd9779
PunkDevloPer <juandavidhernandezayala@gmail.com>
2023-09-21 22:53:56 +0200

    modificacion del js y el menu de inicio de la aplicacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,42 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var consoleText = document.querySelector(".console");
-    var consoleText2 = document.querySelector(".console-2");
-    var menuItems = document.querySelectorAll(".menu-item");
+    var consoleElement = document.getElementById("console");
+    var menuContainer = document.getElementById("menu"); // Cambiado el selector
 
-    consoleText.addEventListener("animationend", function () {
-        var directoryStructure = `
-        Punkdev@github.io:~$ tree
+    var textToPrint = `
+    Punkdev@github.io:~$ tree
+    ├── home
+    │   ├──index.html
+    │   ├── Blog.html
+    │   ├── Plataformas
+    │       ├── HackmyVM.html
+    │       ├── Tryhackme.html
+    │       ├── vulnhub.html
+    │       ├── Hackthebox.html   
+    ├── LINUX
+    │     ├── book.html
+    ├── PYTHON
+    │     ├── book.html
+    ├── usr
+    │    ├── Sobre mi
+    │    ├── cv.html
+    Punkdev@github.io:~$ cd Menu..
+    
+    
+    `;
 
-        ├── home
-        │   ├──index.html
-        │   ├── Blog.html
-        │   ├── Plataformas
-        │       ├── HackmyVM.html
-        │       ├── Tryhackme.html
-        │       ├── vulnhub.html
-        │       ├── Hackthebox.html   
-        ├── LINUX
-        │     ├── book.html
-        ├── PYTHON
-        │     ├── book.html
-        ├── usr
-        │    ├── Sobre mi
-        │    ├── cv.html
-        Punkdev@github.io:~$ cd Blog.html`;
-        typeMessage(directoryStructure, 0, consoleText, function () {
-            menuItems.forEach(function (item, index) {
-                item.style.animationDelay = (index * 0.5) + "s";
-                item.style.display = "inline-block";
-            });
-            // Redireccionar a una página después de que termine la animación
+    var index = 0;
+
+    var interval = setInterval(function () {
+        consoleElement.textContent += textToPrint[index];
+        index++;
+
+        if (index === textToPrint.length) {
+            clearInterval(interval);
             setTimeout(function () {
-                window.location.href = "/Blog.html";
-            }, 1000);
-        });
-    });
-
-    function typeMessage(message, index, element, callback) {
-        if (index < message.length) {
-            element.textContent += message.charAt(index);
-            index++;
-            setTimeout(function () {
-                typeMessage(message, index, element, callback);
-            }, 30);
-        } else {
-            callback();
+                menuContainer.style.display = "block"; // Mostrar el menú
+            }, 500);
         }
-    }
+
+        consoleElement.scrollTop = consoleElement.scrollHeight;
+    }, 30);
 });
